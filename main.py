481f1155d295daea5be9ae4eda5d10c43b7bcd21
Phat0403 <dangtanphat9a1@gmail.com>
2022-12-22 09:02:45 +0700

    g

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ class Button():
 	def checkForInput(self, position):
 		startgame=GameHorse()
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			option_horse()
 			startgame.start_new_round()
-
 	def changeColor(self, position):
 		main_font = pygame.font.SysFont("cambria", 30)
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
@@ -42,10 +42,11 @@ class Button():
 
 
 class Horse(pygame.sprite.Sprite):
-	def __init__(self, pos_x, pox_y, type):
+	def __init__(self, pos_x, pos_y, type,ran):
 		super().__init__()
 		self.type = type
-		
+		self.speed=ran
+		self.animate_fps = .2
 		self.sprites = []
 		for i in range(7):
 			image = pygame.image.load(f"./asset/character/horse1/{self.type}/{i + 1}.png")
@@ -55,14 +56,22 @@ class Horse(pygame.sprite.Sprite):
 		
 		self.current_sprite = 0
 		self.image = self.sprites[self.current_sprite]
-		self.rect = self.image.get_rect(topleft=(pos_x, pox_y))
+		self.rect = self.image.get_rect()
+		self.rect.bottomleft = (pos_x, pos_y)
+		self.positonx = pos_x
 	
-	def update(self, speed):
-		self.rect.x += 1
-		
-		self.current_sprite += speed
-		if self.current_sprite >= len(self.sprites):
+	def update(self):
+		if self.rect.right <= WINDOW_WIDTH:
+
+			self.positonx += self.speed
+			self.rect.x = int(self.positonx)
+			self.animate(self.animate_fps)
+		 
+	def animate(self, fps):
+		self.current_sprite += fps
+		if self.current_sprite >= 6:
 			self.current_sprite = 0
+			
 		self.image = self.sprites[int(self.current_sprite)]
 
 
@@ -89,7 +98,8 @@ class GameHorse():
 		button_login = pygame.image.load("./asset/button/button.png")
 		button_login = pygame.transform.scale(button_login, (220, 100))
 
-		button = Button(button_login, 600, 200, "LOGIN",display_surface)
+		button = Button(button_login, 600, 200, "START",display_surface)
+		button_opt=Button(button_login, 600, 400, "OPTION",display_surface)
 		main_menu_run = True
 		while main_menu_run:
 			display_surface.blit(self.back_ground_image, (0, 0))
@@ -100,6 +110,8 @@ class GameHorse():
 				if event.type == pygame.MOUSEBUTTONDOWN:
 					button.checkForInput(pygame.mouse.get_pos())
 				button.update()
+				button_opt.changeColor(pygame.mouse.get_pos())
+				button_opt.update()
 				button.changeColor(pygame.mouse.get_pos())
 				pygame.display.update()
 				clock.tick(FPS)
@@ -116,7 +128,8 @@ class GameHorse():
 	def start_new_round(self):
 		moving_sprite = pygame.sprite.Group()
 		for i in range(5):
-			horse = Horse(0, 100 * i, i + 1)
+			ran=random.randint(10,20)/20
+			horse = Horse(0,320+ 80 * i , i + 1,ran)
 			moving_sprite.add(horse)
 		racing = True
 		fps = 1
@@ -125,16 +138,27 @@ class GameHorse():
 				if event.type == pygame.QUIT:
 					pygame.quit()
 					sys.exit()
+			display_surface.blit(self.back_ground_image,(0,0))
 			self.map_x-=1
 			self.show_bg()
 			
-			moving_sprite.update(0.2)
+			moving_sprite.update()
 			moving_sprite.draw(display_surface)
 			
 			pygame.display.flip()
 			clock.tick(60)
 
-
+def option_horse():
+	running=True
+	while running:
+		for event in  pygame.event.get():
+			if event.type==pygame.QUIT:
+				running=False
+			if event.type==pygame.MOUSEBUTTONDOWN and event.button==1:
+				if pygame.Rect().collidepoint(event.pos):
+					pass
+		display_surface.blit( pygame.transform.scale( pygame.image.load("./asset/img/backround.png").convert(), (1200, 700)),(0,0))
+		pygame.display.update()
 pygame.init()
 my_game = GameHorse()
 my_game.main()
